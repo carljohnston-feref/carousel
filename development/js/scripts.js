a0c39d03de0ade52faf92c45bf20fcc181e39037
Carl Johnston <randombob@gmail.com>
2016-11-22 10:57:29 +0000

    Reset currentSlide at first / last slide

--- development/js/scripts.js
+++ development/js/scripts.js
@@ -1,1 +1,32 @@
-console.log('log');
+// Variables
+var currentSlide = 0;																		// Set initial slide position
+var imgWidth = document.getElementById('imgContainer').children[0].offsetWidth;				// Find width of first image
+var imgTotal = document.getElementById('imgContainer').getElementsByTagName('img').length;	// Find number of images withing carousel
+
+// prevBtn function
+function prevBtn() {
+	currentSlide++;															// Iterate current slide
+
+	if (currentSlide > 0) {													// If currentSlide is showing the first image position
+		currentSlide = (imgTotal - (imgTotal * 2)) + 1;						// Set currentSlide to the last image position
+	}
+
+	var slideAnim = (imgWidth * currentSlide);								// Calculate animation distance (#px from left)
+	document.getElementById('imgContainer').style.left = slideAnim + "px";	// Animate container
+}
+
+// nextBtn function
+function nextBtn() {
+	currentSlide--;															// Iterate current slide
+
+	if (currentSlide <= imgTotal - (imgTotal * 2)) {						// If currentSlide is showing the last image position
+		currentSlide = 0;													// Set currentSlide to the first image position
+	}
+
+	var slideAnim = (imgWidth * currentSlide);								// Calculate animation distance (#px from left)
+	document.getElementById('imgContainer').style.left = slideAnim + "px";	// Animate container
+}
+
+// Controls Event Listeners
+document.getElementById('prev').addEventListener('click', prevBtn, false);
+document.getElementById('next').addEventListener('click', nextBtn, false);
